80ff9a44bdf6aab021df8cbedfa3624928008adc
lei.zhu <lei.zhu@yelopack.com>
2020-11-11 15:43:54 +0800

    ai智能后台初建，并起天猫精灵项目

---
+++ ai-tmall/src/main/java/com/zl/ai/tmall/TmallAIApplication.java
@@ -0,0 +1,17 @@
+package com.zl.ai.tmall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:lei.zhu
+ * @date:2020/11/11 14:41
+ */
+@SpringBootApplication
+public class TmallAIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TmallAIApplication.class);
+    }
+}
+
